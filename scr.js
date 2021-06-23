d360a3f805a6848d2d2bb94208122ca4425d34d5
Sagnik Das <sagnikdas53@live.com>
2021-06-23 12:27:00 +0530

    added cotrols

--- scr.js
+++ scr.js
@@ -1,11 +1,12 @@
+var stop = true;
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer({
-    canvas: document.querySelector('#bg'),
-  });
+  canvas: document.querySelector('#bg'),
+});
 
-  
+
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
@@ -30,42 +31,42 @@ const lightHelper = new THREE.PointLightHelper(pointLight)
 const gridHelper = new THREE.GridHelper(200, 50);
 scene.add(lightHelper, gridHelper)
 
-function animate(){
-    requestAnimationFrame(animate);
+function animate() {
+  requestAnimationFrame(animate);
+  if (stop) {
     /*torus.rotation.x += 0.01;
     torus.rotation.y += 0.03;
     torus.rotation.z += 0.04;
-
     fubuker.position.z -= 0.2;
     fubuker.position.x += 0.2;
     fubuker.position.y += 0.2;*/
-
     fubuker.rotation.x += 0.05;
     fubuker.rotation.y += 0.05;
     fubuker.rotation.z += 0.05;
     renderer.render(scene, camera);
+  }
 }
 
 animate()
 
 
 function moveCamera() {
-    const t = document.body.getBoundingClientRect().top;
-    /* 
-    fubuker.position.z = t * -0.01;
-    fubuker.position.x = t * -0.0002;
-    fubuker.position.y = t * -0.0002;
-    */
-    camera.position.z = t * -0.01;
-    camera.position.x = t * -0.0002;
-    camera.position.y = t * -0.0002;
-    //console.log(camera.position.x,camera.position.y,camera.position.z);
-  }
-  
+  const t = document.body.getBoundingClientRect().top;
+  /* 
+  fubuker.position.z = t * -0.01;
+  fubuker.position.x = t * -0.0002;
+  fubuker.position.y = t * -0.0002;
+  */
+  camera.position.z = t * -0.01;
+  camera.position.x = t * -0.0002;
+  camera.position.y = t * -0.0002;
+  //console.log(camera.position.x,camera.position.y,camera.position.z);
+}
+
 document.body.onscroll = moveCamera;
 moveCamera();
 
-window.onload = function() {
-  //document.getElementById("my_audio").play();
-  //window.scroll(0,window.innerHeight/2);
+window.onload = function () {
+  document.getElementById("my_audio").play();
+  window.scroll(0, window.innerHeight / 2);
 }
